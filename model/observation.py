c8fe5b9621e4f37733119bd05a958450c8b853db
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-09 20:53:23 +0100

    Add __repr__

--- model/observation.py
+++ model/observation.py
@@ -26,3 +26,5 @@ class Observation:
         """
         return ','.join([str(value) for value in self.__dict__.values()])
 
+    def __repr__(self):
+        return self.get_values_comma_delimited()
