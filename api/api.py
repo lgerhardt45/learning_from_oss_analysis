3539f60facfacbe87eef28da2957cf8b646e0537
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-11 20:37:22 +0100

    small bugfix where not the full url was used for v3 get request

--- api/api.py
+++ api/api.py
@@ -14,8 +14,9 @@ class API:
         Makes an authorized GET requests to Github's v3 API (https://api.github.com) adding authorization header
         :returns the full response from `requests.get()`
         """
+
         full_url = self._github_v3_api_base_url + url
-        response = requests.get(url=url, headers={'Authorization': 'token %s' % self._api_token})
+        response = requests.get(url=full_url, headers={'Authorization': 'token %s' % self._api_token})
         print('Getting %s, response code: %i' % (url, response.status_code))
         if not response:
             print('Failed request: %s' % response.json())
