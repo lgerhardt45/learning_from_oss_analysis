e3fe3358ad967724e14dee4287b39dfc4221e1a5
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-11 17:42:48 +0100

    Move setup() to and restructure main.py

--- contributor_analysis.py
+++ contributor_analysis.py
@@ -17,20 +17,6 @@ class ContributorAnalysis:
         self.setup()
 
     # UTILITY METHODS
-    def setup(self):
-        print('setting up')
-        # get config
-        with open('config.json') as config:
-            config_json = json.load(config)
-            # api_token for authorization
-            self._api_token = config_json['api_token']
-            util.api_token = self._api_token
-            # only first n observations for debugging purposes
-            self._slice_amount = config_json['debug_slice_amount']
-            # setup output file
-            self._output_file_path = config_json['output_file_name']
-        print('done setting up')
-
 
     def save_contributor_stats_to_json(self):
         if os.path.exists(util.json_file_path):
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from contributor_analysis import ContributorAnalysis
 from csv_writer import CSVWriter
+import util as util
 
-if __name__ == '__main__':
+
+def setup(self):
+    print('setting up')
+    # get config
+    with open('config.json') as config:
+        config_json = json.load(config)
+        # api_token for authorization
+        self._api_token = config_json['api_token']
+        util.api_token = self._api_token
+        # only first n observations for debugging purposes
+        self._slice_amount = config_json['debug_slice_amount']
+        # setup output file
+        self._output_file_path = config_json['output_file_name']
+    print('done setting up')
+
+
+def main():
     ContributorAnalysis().run()
     CSVWriter().run
 
+
+if __name__ == '__main__':
+    main()
