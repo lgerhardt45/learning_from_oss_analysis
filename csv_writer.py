8946a96d4cf3cb7990ea710cc7f11816747ea161
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-11 17:38:18 +0100

    [in dev] run CSV writer from main.py

--- csv_writer.py
+++ csv_writer.py
@@ -7,9 +7,13 @@ from model.observation import Observation
 
 class CSVWriter:
 
-    def __init__(self, observations: []):
+    def __init__(self, observations: [Observation]):
         self._observations = observations
         self._output_file_path = util.output_file_path
+
+    def run(self):
+        self.setup_output_file()
+        self.export_observations_to_csv()
 
     def setup_output_file(self):
         print('setting up output file')
@@ -28,16 +32,17 @@ class CSVWriter:
             try:
                 # write header
                 csv_file.write(self._observations[0].get_attribute_names_comma_delimited() + '\n')
-                print('done ')
-            except IOError:
-                print('Cannot write to output file')
+                print('done setting up output file')
+            except IOError as ioe:
+                print('cannot write to output file: %s' % ioe)
                 sys.exit(1)
 
     def export_observations_to_csv(self):
         for observation in self._observations:
-            with open(self._output_file_path, mode='a') as csv:
+            with open(self._output_file_path, mode='a') as csv_file:
                 try:
-                    csv.write(observation.get_values_comma_delimited() + '\n')
-                except IOError as e:
-                    print(e)
+                    csv_file.write(observation.get_values_comma_delimited() + '\n')
+                except IOError as ioe:
                     print('failed to write %s to %s' % (repr(observation), self._output_file_path))
+                    print(ioe)
+                    sys.exit(1)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from contributor_analysis import ContributorAnalysis
+from csv_writer import CSVWriter
 
 if __name__ == '__main__':
     ContributorAnalysis().run()
+    CSVWriter().run
+
