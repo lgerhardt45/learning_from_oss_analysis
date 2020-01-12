72988345ba2785db89c8f5814fd7ef1121630ca6
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-12 16:34:44 +0100

    read observations file output path from config.json

--- data_analysis/data_analysis.R
+++ data_analysis/data_analysis.R
@@ -1,2 +1,7 @@
-# install.packages("data.table") # uncomment if not installed yet
+# install.packages("data.table") # uncomment if needed
+# install.packages("rjson")      # uncomment if needed
 library(data.table)
+library(rjson)
+
+config <- fromJSON(file = "config.json")
+observations_file_path = config$output_file_path
