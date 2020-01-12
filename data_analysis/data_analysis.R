7a1c6ef43d8895f0adf2fefe2f490ccc58acdd65
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-01-12 16:35:11 +0100

    Import data as data frame and print first summary

--- data_analysis/data_analysis.R
+++ data_analysis/data_analysis.R
@@ -5,3 +5,10 @@ library(rjson)
 
 config <- fromJSON(file = "config.json")
 observations_file_path = config$output_file_path
+
+# import data
+full_observations_file_path <- paste(getwd(), observations_file_path ,sep = "/")
+observations <- read.csv(full_observations_file_path, header=TRUE, sep = ";", dec = ",")
+observations_dt <- as.data.table(observations)
+class(observations_dt)
+summary(observations_dt)
